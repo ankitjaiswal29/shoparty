9fde702184d5b2ccfda3fc08c66a4cce2be058ae
subhrajitdeb <subhrajitdeb54@gmail.com>
2022-01-13 18:24:34 +0530

    bottom navigation

--- app/src/main/java/com/example/shoparty_android/MainActivity.kt
+++ app/src/main/java/com/example/shoparty_android/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.shoparty_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_fragment)
+        bottom_navigatin_view.setupWithNavController(navController)
     }
 }
