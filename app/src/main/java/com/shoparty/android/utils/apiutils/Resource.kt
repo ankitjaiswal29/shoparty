f1818a0118509db9a6a4fde4ed7ecc69ff3ecb47
Ankit jaiswal <ankit.jaiswal@kratin.co.in>
2022-08-08 18:29:07 +0530

    all

--- app/src/main/java/com/shoparty/android/utils/apiutils/Resource.kt
+++ app/src/main/java/com/shoparty/android/utils/apiutils/Resource.kt
@@ -3,9 +3,7 @@ package com.shoparty.android.utils.apiutils
 
 sealed class Resource<T>(
     val message: String? = null,
-    val data: T? = null
-
-) {
+    val data: T? = null) {
     class Success<T>(message: String, data: T? = null) : Resource<T>(message, data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(message, data)
     class Loading<T> : Resource<T>()
